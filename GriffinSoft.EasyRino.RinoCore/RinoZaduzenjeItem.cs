e887735d1a12cb621688ea48d17c95ea08d5c771
Dusan Misic <promisic@outlook.com>
2016-09-13 12:43:40 +0200

    Initial commit of EasyRino project to Mercurial SCM.

---
+++ GriffinSoft.EasyRino.RinoCore/RinoZaduzenjeItem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriffinSoft.EasyRino.RinoCore
+{
+    public class RinoZaduzenjeItem
+    {
+        /// <summary>
+        /// Action type enumeration.
+        /// </summary>
+        internal enum ActionType
+        {
+            Unos,
+            Izmena,
+            Obrisi
+        }
+
+        /// <summary>
+        /// Property holds unique RINO ID.
+        /// </summary>
+        internal long RinoId { get; set; }
+
+        /// <summary>
+        /// Property holds ID of the document.
+        /// </summary>
+        internal string BrojDokumenta { get; set; }
+
+        /// <summary>
+        /// Property holds PIBPoverioca.
+        /// </summary>
+        internal string PIBPoverioca { get; set; }
+
+        /// <summary>
+        /// Property holds bank information.
+        /// </summary>
+        internal string Banka { get; set; }
+
+        /// <summary>
+        /// Property holds ReklPodZaRek.
+        /// </summary>
+        internal string ReklPodZaRek { get; set; }
+
+        /// <summary>
+        /// Property holds date of payment.
+        /// </summary>
+        internal DateTime DatumIzmirenja { get; set; }
+
+        /// <summary>
+        /// Property holds ammount.
+        /// </summary>
+        internal decimal Iznos { get; set; }
+
+        /// <summary>
+        /// Property holds reason for change.
+        /// </summary>
+        internal string RazlogIzmene { get; set; }
+
+        /// <summary>
+        /// Checks if PIB is valid.
+        /// </summary>
+        /// <returns>True if PIB is 9 characters long, false if otherwise.</returns>
+        internal bool IsPibValid()
+        {
+            // Checking if PIB is 9 characters long
+            if (this.PIBPoverioca.Length == 9)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
